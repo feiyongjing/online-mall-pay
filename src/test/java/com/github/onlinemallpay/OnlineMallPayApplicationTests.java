f59910369dd1fe0dee0b3651750012fb829641c9
feiyongjing <2059326740@qq.com>
2021-08-23 15:41:03 +0800

    初始化项目

---
+++ src/test/java/com/github/onlinemallpay/OnlineMallPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.onlinemallpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineMallPayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
